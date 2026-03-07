cd81434cfb551b2d0f4c40e7a7cb49f0997ad93e
sushma <sushma.ganji002@gmail.com>
2026-03-07 13:11:43 +0530

    Created the structure of the project

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Category from './components/Category'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
+import Insta from './components/Insta'
+import Productsgrid from './components/Productsgrid'
+import Reviews from './components/Reviews'
+import Services from './components/Services'
+import Types from './components/Types'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Category />
+      <Types />
+      <Services />
+      <Productsgrid />
+      <Banner />
+      <Reviews />
+      <Insta />
+      <Footer />
+
+
+
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Productsgrid.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Productsgrid = () => {
+    return (
+        <div>
+            productsgrid
+        </div>
+    )
+}
+
+export default Productsgrid
